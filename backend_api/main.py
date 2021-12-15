29dc268c2188a0a20b32a2566eab7340179be33e
utkuc99 <utkuc1999@gmail.com>
2021-12-15 14:14:07 +0300

    added gpu limit

--- backend_api/main.py
+++ backend_api/main.py
@@ -25,6 +25,17 @@ def download_video_srt(videoCode):
 
 if __name__ == '__main__':
 
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only use the first GPU
+        try:
+            tf.config.set_visible_devices(gpus[0], 'GPU')
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
+
 
     parser = argparse.ArgumentParser()
     parser.add_argument("videoCode", help="give youtube video code")
