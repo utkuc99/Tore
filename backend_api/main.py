431b9f148443f7980f2251f0b6c6c433c6858f78
utkuc99 <utkuc1999@gmail.com>
2021-12-15 14:26:45 +0300

    added memory limitter

--- backend_api/main.py
+++ backend_api/main.py
@@ -22,9 +22,21 @@ def download_video_srt(videoCode):
     return movie_filename
 
 
+def memory_limit():
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (get_memory() * 1024 / 2, hard))
+
+def get_memory():
+    with open('/proc/meminfo', 'r') as mem:
+        free_memory = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                free_memory += int(sline[1])
+    return free_memory
+
 
 if __name__ == '__main__':
-
 
     parser = argparse.ArgumentParser()
     parser.add_argument("videoCode", help="give youtube video code")
@@ -33,4 +45,10 @@ if __name__ == '__main__':
 
     videoCode = args.videoCode
     download_video_srt(videoCode)
-    shorten_video(videoCode)
+
+    memory_limit()  # Limitates maximun memory usage to half
+    try:
+        shorten_video(videoCode)
+    except MemoryError:
+        sys.stderr.write('nnERROR: Memory Exceptionn')
+        sys.exit(1)
